b73bc995f9103dd9745cec782ac070a80b8b946c
Fabricio Almeida <fga294@gmail.com>
2023-11-28 22:04:26 +1100

    Pre Authentication

---
+++ blog/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog] = []
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {"date": {"name": "Fabricio"}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f"List of the first {limit} published records"}
+    else:
+        return {"data": f"{limit} rows fetched"}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return id
+
+
+@app.get('/blog/{id}/comments')
+def showcomments():
+    return {"data": {"1", "2"}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {"data":  f'Blog with tilte: {blog.title}, has been created \
+        successfully'}
