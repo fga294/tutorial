7e349b52083314d6b4660cf32b5e922a508084d7
Fabricio Almeida <fga294@gmail.com>
2023-11-30 13:50:40 +1100

    with authentication

--- blog/hashing.py
+++ blog/hashing.py
@@ -6,3 +6,6 @@ pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash:
     def bcrypt(self, password: str):
         return pwd_ctx.hash(password)
+
+    def verify(plain_password, hashed_password):
+        return pwd_ctx.verify(plain_password, hashed_password)
--- blog/schemas.py
+++ blog/schemas.py
@@ -23,3 +23,17 @@ class User(BaseModel):
     name: str
     email: str
     password: str
+
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str | None = None
